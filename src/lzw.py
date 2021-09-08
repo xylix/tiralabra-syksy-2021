890626990264bd4cbf0d0ac9bd1c0efe072e90ec
Kerkko Pelttari <kerkko@robocorp.com>
2021-09-08 21:44:00 +0300

    Add a test input file and a CLI to lzw.py

--- src/lzw.py
+++ src/lzw.py
@@ -1,3 +1,12 @@
+from typing import Dict
+import logging
+import typer
+
+
+TESTFILE_1 = "test_input_1.txt"
+TESTFILE_1_START_DICT = {"a": 0, "b": 1, "d": 2, "n": 3, "_": 4}
+
+
 """ Pseudocode from https://www2.cs.duke.edu/csed/curious/compression/lzw.html
 
 string s;
@@ -23,8 +32,38 @@ encode s to output file;
 
 """
 
-def compress():
+
+def compress(filename):
+    """
+    Creates a file and a dict, named {filename}.lzw and {filename}.lzwdict
+    """
+    output = ""
+    # dictionary: Dict[str, str] = {}
+    dictionary = TESTFILE_1_START_DICT
+    with open(filename, "r") as f:
+        bytes = f.read()
+        s = ""
+        for i, ch in enumerate(bytes):
+            logging.debug(f"character at index {i}: {ch}")
+            logging.debug(f"s+ch ({s+ch}) in dict: {dictionary.get(s+ch)}")
+
+            if dictionary.get(s + ch):
+                s = s + ch
+            else:
+                output += str(dictionary[s])
+                # TODO: this might be incorrect
+                dictionary[s + ch] = len(dictionary.items())
+                s = ch
+    logging.debug(f"output: {output}")
+    logging.debug(f"dict: {dictionary}")
+    with open(f"{filename}.lzw", "w") as outf:
+        outf.write(output)
+
+    # this is not necessary; dict can be built during decoding https://en.wikipedia.org/wiki/Lempel%E2%80%93Ziv%E2%80%93Welch#Decoding
+    # with open(f"{filename}.lzwdict") as outdict:
+    # outdict.write(dictionary)
     pass
+
 
 """ Pseudocode from https://www2.cs.duke.edu/csed/curious/compression/lzw.html
 
@@ -45,11 +84,30 @@ while (there is still data to read)
     prevcode = currcode;
 }
 """
-def decompress():
+
+
+def decompress(filename: str):
     pass
 
-def main():
-    print("hello world")
+
+def main(
+    filename: str,
+    archive: bool = True,
+    extract: bool = False,
+    debug: bool = True,
+):
+    if archive and extract:
+        raise Exception("Either archive or extract, not both")
+    if debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.WARNING)
+    # logging.debug(f"Argument List: {sys.argv}")
+    if archive:
+        compress(filename)
+    elif extract:
+        decompress(filename)
+
 
 if __name__ == "__main__":
-    main()
+    typer.run(main)
