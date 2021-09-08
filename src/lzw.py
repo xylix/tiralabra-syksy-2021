ab889d6e38d6dcc1bb7ab88f1fc21f81bc7a807a
Kerkko Pelttari <kerkko@robocorp.com>
2021-09-08 16:32:44 +0300

    Add lzw pseudocode examples

--- src/lzw.py
+++ src/lzw.py
@@ -1,3 +1,53 @@
+""" Pseudocode from https://www2.cs.duke.edu/csed/curious/compression/lzw.html
+
+string s;
+char ch;
+...
+
+s = empty string;
+while (there is still data to be read)
+{
+    ch = read a character;
+    if (dictionary contains s+ch)
+    {
+	s = s+ch;
+    }
+    else
+    {
+	encode s to output file;
+	add s+ch to dictionary;
+	s = ch;
+    }
+}
+encode s to output file;
+
+"""
+
+def compress():
+    pass
+
+""" Pseudocode from https://www2.cs.duke.edu/csed/curious/compression/lzw.html
+
+string entry;
+char ch;
+int prevcode, currcode;
+...
+
+prevcode = read in a code;
+decode/output prevcode;
+while (there is still data to read)
+{
+    currcode = read in a code;
+    entry = translation of currcode from dictionary;
+    output entry;
+    ch = first char of entry;
+    add ((translation of prevcode)+ch) to dictionary;
+    prevcode = currcode;
+}
+"""
+def decompress():
+    pass
+
 def main():
     print("hello world")
 
